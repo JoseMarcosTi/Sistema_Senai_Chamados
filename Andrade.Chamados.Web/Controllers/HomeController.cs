642ec84dffffd85c71ae116690439b9397f45528
Marcos Andrade <andrade.ti@outlook.com>
2018-06-20 20:55:40 -0300

    Apenas confirmando a atualização do PJ

---
+++ Andrade.Chamados.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Andrade.Chamados.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = " Home index controller ";
+            ViewBag.Mensagem = " Hello my friend, welcome! ";
+            return View();
+        }
+    }
+}
