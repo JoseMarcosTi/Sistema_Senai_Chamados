83ff03071036144d29401df1033da78149939aef
Marcos Andrade <andrade.ti@outlook.com>
2018-07-05 22:11:26 -0300

    Aula dia 05/07/2018

--- Andrade.Chamados.Web/Controllers/ChamadoController.cs
+++ Andrade.Chamados.Web/Controllers/ChamadoController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Web.Mvc;
 
 namespace Andrade.Chamados.Web.Controllers
@@ -37,6 +38,46 @@ namespace Andrade.Chamados.Web.Controllers
             }
         }
 
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            ChamadoViewModel vmChamado = new ChamadoViewModel();
+
+            return View(vmChamado);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(ChamadoViewModel chamado)
+        {
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    ViewBag.Erro = " Dados invalidos";
+                    return View(chamado);
+                }
+
+                using (ChamadoRepositorio _repChamado = new ChamadoRepositorio())
+                {
+                    var identity = User.Identity as ClaimsIdentity;
+                    var id = identity.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
+                    _repChamado.Inserir(Mapper.Map<ChamadoViewModel, ChamadoDomain>(chamado));
+                    chamado.IdUsuario = new Guid(id);
+                }
+
+                TempData["Sucesso"] = " Chamado cadastrado...Aguarde!!!";
+                return RedirectToAction("Index");
+            }
+            catch (System.Exception ex)
+            {
+                ViewBag.Erro = ex.Message;
+                return View(chamado);
+            }
+        }
 
     }
 }
--- Andrade.Chamados.Web/ViewModels/Chamado/ChamadoViewModel.cs
+++ Andrade.Chamados.Web/ViewModels/Chamado/ChamadoViewModel.cs
@@ -2,6 +2,7 @@
 using Andrade.Chamados.Web.ViewModels.Usuario;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,15 +11,69 @@ namespace Andrade.Chamados.Web.ViewModels.Chamado
 {
     public class ChamadoViewModel : BaseViewModel
     {
+
+        public ChamadoViewModel()
+        {
+            ListaSetores = CarregaListaSetores();
+            ListaStatus = CarregaListaStatus();
+        }
+
+        [Required(ErrorMessage="Título deve ser preenchido")]
+        [Display(Name ="Título do chamado")]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição do chamado")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe o setor")]
         public EnSetor Setor { get; set; }
+
+        [Required(ErrorMessage = "Informe o status")]
         public EnStatus Status { get; set; }
+
         public Guid IdUsuario { get; set; }
 
         public virtual UsuarioViewModel Usuario { get; set; }
 
         public SelectList ListaSetores { get; set; }
         public SelectList ListaStatus { get; set; }
+
+
+        /// <summary>
+        /// Carrega lista de setores a partir de um Enum
+        /// </summary>
+        /// <returns> Retorna um SelectList com os setores </returns>
+        public SelectList CarregaListaSetores()
+        {
+            var listaSetores = new SelectList(Enum.GetValues(typeof(EnSetor)).Cast<EnSetor>().Select(c =>
+            new SelectListItem
+            {
+                Text = c.ToString(),
+                Value = ((int)c).ToString()
+           }).ToList(), "Value" , "Text");
+
+            return ListaSetores;
+        }
+
+
+        /// <summary>
+        /// Carrega lista de status a partir de Enum
+        /// </summary>
+        /// <returns> Retorna um SelectList com os status </returns>
+        public SelectList CarregaListaStatus()
+        {
+            var listaStatus = new SelectList(Enum.GetValues(typeof(EnStatus)).Cast<EnStatus>().Select(c =>
+            new SelectListItem
+            {
+                Text = c.ToString(),
+                Value = ((int)c).ToString()
+            }).ToList(), "Value", "Text");
+
+            return ListaStatus;
+        }
+
+
+
     }
 }
