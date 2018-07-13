bcf361b6d5422cd78242b9a22791dc3e1d12d821
Marcos Andrade <andrade.ti@outlook.com>
2018-07-12 21:49:48 -0300

    Criação de Helpers Aula_12/07/2018

---
+++ Andrade.Chamados.Web/Helpers/MyHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Andrade.Chamados.Web.Helpers
+{
+    public class MyHelper
+    {
+        public static string DireitosReservados()
+        {
+            return DateTime.Now.Year + " - Andrade - Todos os direitos reservados";
+        }
+
+        public static string BoasVindas()
+        {
+            return "Seja bem vindo, ";
+        }
+    }
+}
